8bbf0d4de23acc9fd3f025ad41ebd2f32eff8276
repon <repon27@yahoo.co.jp>
2024-12-12 16:24:14 +0900

    Svelte編のコードを移植

---
+++ src/routes/products/Slider.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+  export let images: string[];
+  // スライダー
+  let centerIndex = 0;
+  $: leftIndex = (centerIndex - 1 + images.length) % images.length
+  $: rightIndex = (centerIndex + 1) % images.length
+
+  const sliderMoveLeft = () => {
+    const length = images.length
+    centerIndex = (centerIndex - 1 + length) % length
+  }
+
+  const sliderMoveRight = () => {
+    const length = images.length
+    centerIndex = (centerIndex + 1) % length
+  }
+
+</script>
+
+<div class="slider">
+  <img src={images[leftIndex]} alt="スライダー画像（左）" class="slider-item left" />
+  <img src={images[centerIndex]} alt="スライダー画像" class="slider-item" />
+  <img src={images[rightIndex]} alt="スライダー画像（右）" class="slider-item right" />
+  <button class="slider-left-button" on:click={sliderMoveLeft}>←</button>
+  <button class="slider-right-button" on:click={sliderMoveRight}>→</button>
+</div>
+
+<style>
+    /*slider*/
+    .slider {
+    position: relative;
+    width: 80%;
+    margin: 0 10%;
+  }
+
+  .slider-item {
+    width: 100%;
+  }
+
+  .slider-item.left{
+    position: absolute;
+    top: 0;
+    right: 100%;
+  }
+
+  .slider-item.right {
+    position: absolute;
+    top: 0;
+    left: 100%;
+  }
+
+  .slider-left-button {
+    position: absolute;
+    top: 50%;
+    right: 100%;
+  }
+
+  .slider-right-button {
+    position: absolute;
+    top: 50%;
+    left: 100%;
+  }
+</style>
